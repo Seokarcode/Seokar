d1541465d5b8fcb454d547b55375f3dd88af45f8
Sajjad akbari <sajjadakbariiii415@gmail.com>
2025-02-04 03:14:04 +0330

    Create seokar-dashboard.php

---
+++ Admin/seokar-dashboard.php
@@ -0,0 +1,11 @@
+<?php
+// Dashboard page for Seokar plugin
+
+function seokar_dashboard_page() {
+    ?>
+    <div class="wrap">
+        <h1>داشبورد سئوکار</h1>
+        <p>خوش آمدید به داشبورد سئوکار.</p>
+    </div>
+    <?php
+}
